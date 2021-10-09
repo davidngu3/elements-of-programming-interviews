35f00475f0007ec2c784d71664c7375c76678494
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-09 20:22:23 +1100

    set up cases for prob5.1

---
+++ prob5-1.java
@@ -0,0 +1,26 @@
+class Problem5_1 {
+    public void main(String[] args) {
+        System.out.println("Start");
+        long example1 = 25;
+        parityBruteForce(example1);
+        parityBruteForceOptimized(example1);
+        parityCaching(example1);
+        paritySimultaneous(example1);
+    }
+
+    public void parityBruteForce(long input) {
+        
+    }
+
+    public void parityBruteForceOptimized(long input) {
+
+    }
+
+    public void parityCaching(long input) {
+
+    }
+
+    public void paritySimultaneous(long input) {
+
+    }
+}
