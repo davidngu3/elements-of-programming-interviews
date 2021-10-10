f781f2f40a492177d41b7519aa42e76f07d6c533
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-10 11:37:40 +1100

    add template class

---
+++ probX.java
@@ -0,0 +1,15 @@
+class ProblemX {
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+        Description:
+        
+        Complexity:
+        
+    */
+    public static void method1(int x) {
+        return;
+    }
+}
