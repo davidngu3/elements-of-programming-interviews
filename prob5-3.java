73297316dca2bc7c5d7a442dd8bd6a06dd2c0f74
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-11 14:28:47 +1100

    5-3 reverse bits

---
+++ prob5-3.java
@@ -0,0 +1,16 @@
+class Prob5_3 {
+    public static void main(String[] args) {
+        long ex1 = 0b101110;
+        System.out.println(Long.toBinaryString(reverseBits(ex1)));
+    }
+
+    /*
+        Description:
+       
+        Complexity: 
+        
+    */
+    public static long reverseBits(long x) {
+        return;
+    }
+}
