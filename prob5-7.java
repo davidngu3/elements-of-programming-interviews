5d8bafa17db6a548eb41f1c932e437e1eba26677
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-12 07:41:33 +1100

    prob 5-7 recursion

--- prob5-7.java
+++ prob5-7.java
@@ -2,20 +2,60 @@
 
 class Problem5_7 {
     public static void main(String[] args) {
-        System.out.println(exponentiation_bruteforce(2, 3));
+       //  System.out.println(exponentiation_bruteforce(2, 3));
+        System.out.println(exponentiation(3, 3));
     }
 
     /*
         Description:
-        
+            Repeatedly multiply by x
         Complexity:
-        
+            O(2^n), where n is the number of bits in the integer type
     */
-    public static long exponentiation_bruteforce(int x, int y) {
-        int res = 1;
+    public static double exponentiation_bruteforce(double x, int y) {
+        double res = 1;
         for (int i = 0; i < y; i++) {
             res *= x;
         }
         return res;
+    }
+
+    /*
+        Description:
+            Repeatedly square the result
+            Check LSB for even/odd
+            If odd, then multiply by x too
+
+            If y is negative, then we need to do 1/x instead (and reverse the sign on y)
+
+        Complexity:
+            O(N)
+        
+    */
+    public static double exponentiation(double x, int y) {
+        long power = y;
+
+        // negative exponent case
+        if (y < 0) {
+            x = 1/x;
+            power = -power;
+        }
+
+        return exponentiation_aux(x, power);
+    }
+    
+    public static double exponentiation_aux(double x, long power) {
+        // power is 0
+        if (power == 0) {
+            return 1;
+        }
+        
+        // power is odd
+        if ((power & 1) == 1) {
+            return x * exponentiation_aux(x, power >>> 1) * exponentiation_aux(x, power >>> 1);
+        }
+
+        // power is even
+        return exponentiation_aux(x, power >>> 1) * exponentiation_aux(x, power >>> 1);
     }
 }
