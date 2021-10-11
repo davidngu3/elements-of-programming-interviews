7bd795709651fb615e2263d9a6281e4de3103a0e
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-11 14:18:35 +1100

    more elegant way to create bitmasks (without math module)

--- prob5-2.java
+++ prob5-2.java
@@ -1,12 +1,13 @@
 class Problem5_2 {
     public static void main(String[] args) {
         long ex1 = 0b101110;
+        // System.out.println(Long.toBinaryString(swapBits(ex1, 1, 4)));
         System.out.println(Long.toBinaryString(swapBits(ex1, 4, 5)));
     }
 
     /*
         Description:
-      
+
         Complexity: 
         
     */
@@ -14,13 +15,14 @@ class Problem5_2 {
         int ival = (int) ((x >>> i) & 1);
         int jval = (int) ((x >>> j) & 1);
 
-        int b = ival ^ jval;
-        if (b == 0) {
+        if (ival == jval) {
             return x; // no swap necessary;
         }
         
-        // swap i and j using mask
-        long mask = (long) Math.pow(2, i) | (long) Math.pow(2, j);
-        return x ^ mask;
+        // toggle values of i and j using a mask
+        long mask = (1L << i) | (1L << j);
+        x ^= mask;
+        
+        return x;
     }
 }
