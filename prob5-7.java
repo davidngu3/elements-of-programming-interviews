2e2498059e911fddffdfb696a3d0362c26824d22
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-12 07:07:14 +1100

    5-7 brute force exponentiation

--- prob5-7.java
+++ prob5-7.java
@@ -1,6 +1,8 @@
+// Write a program that takes a double x and an integer y and returns x^y. You can ignore overflow and underflow.
+
 class Problem5_7 {
     public static void main(String[] args) {
-        
+        System.out.println(exponentiation_bruteforce(2, 3));
     }
 
     /*
@@ -9,7 +11,11 @@ class Problem5_7 {
         Complexity:
         
     */
-    public static void method1(int x) {
-        return;
+    public static long exponentiation_bruteforce(int x, int y) {
+        int res = 1;
+        for (int i = 0; i < y; i++) {
+            res *= x;
+        }
+        return res;
     }
 }
