94ef19389250a3ef01af7ab4ccaaf670a1f3e09c
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-14 09:15:24 +1100

    6

---
+++ prob6-17.java
@@ -0,0 +1,16 @@
+class Prob6_17 {
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+        Description:
+        
+        Complexity: 
+           
+        
+    */
+    public static void method(int[] arr) {
+       
+    }
+}
---
+++ prob6-6.java
@@ -0,0 +1,16 @@
+class Prob6_6 {
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+        Description:
+        
+        Complexity: 
+           
+        
+    */
+    public static void method(int[] arr) {
+       
+    }
+}
