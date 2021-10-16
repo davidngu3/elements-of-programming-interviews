a2ed7e00cb6539134a1076d20c0d8c0e624d4308
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-16 13:59:56 +1100

    6-5 delete duplicates using 2 pointers

--- prob6-5.java
+++ prob6-5.java
@@ -5,18 +5,29 @@ import java.util.Collections;
 
 class Prob6_5 {
     public static void main(String[] args) {
-        
+        deleteDuplicates(new ArrayList<Integer>(List.of(1, 1, 2, 3, 4, 4, 4, 7)));
     }
 
     /*
         Description:
+            2 pointers, one pointer traverses the list, one points at index to place next distinct integer.
 
         Complexity: 
+            O(N) time, O(1) space
            
         
     */
     public static void deleteDuplicates(List<Integer> sortedArr) {
-        
- 
+        int i = 1; // stores pos of next distinct int
+        int j = 1; // iterates array, checking adjacent elements for duplicates
+
+        while (j < sortedArr.size()) {
+            if (sortedArr.get(j) != sortedArr.get(j-1)) {
+                sortedArr.set(i, sortedArr.get(j));
+                i++;
+            }
+            j++;
+        }
+
     }
 }
