1fad2c22d7de992395b817bd2e5da024460ac61c
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-16 13:43:12 +1100

    add prob 6-5

---
+++ prob6-5.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Collections;
+
+class Prob6_5 {
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+        Description:
+
+        Complexity: 
+           
+        
+    */
+    public static void deleteDuplicates(List<Integer> sortedArr) {
+        
+ 
+    }
+}
