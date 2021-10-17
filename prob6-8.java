be778bb6e2d1a981cbfb9658609d209fa68a09fc
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-17 14:01:42 +1100

    cleanup

--- prob6-8.java
+++ prob6-8.java
@@ -15,26 +15,36 @@ class Prob6_8 {
                 So maintain a cache of all integers 0 to n, marking all valid multiples in range and use it to quickly rule out non-primes
 
         Complexity: 
+            Time: O(n log log n)
+            Space: O(n)
            
         
     */
-    public static void enumerate_primes(int n) {
+    public static List<Integer> enumerate_primes(int n) {
         List<Integer> cache = new ArrayList<Integer>(Collections.nCopies(n+1, 1)); // array of n+1 ones. zero indicates not prime
+        cache.set(0, 0);
+        cache.set(1, 0);
+
+        List<Integer> primes = new ArrayList<Integer>(); 
 
         for (int i = 2; i <= n; i++) {
+
             if (cache.get(i) == 1) {
+                // prime candidate added to result
+                primes.add(i);
+                
+                // sift out multiples
                 for (int j = i+i; j <= n; j += i) {
                     cache.set(j, 0);
                 }
             }
         }
 
-        List<Integer> primes = new ArrayList<Integer>(); 
-        for (int x = 2; x <= n; x++) {
-            if (cache.get(x) == 1) {
-                primes.add(x);
-            }
+        for (int x : primes) {
+            System.out.println(x);
         }
+
+        return primes;
     }
 
 }
