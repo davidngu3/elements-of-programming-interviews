ec301b48efa522b8e76ef329bcd6565f8216d414
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-19 07:58:23 +1100

    create prob 7

---
+++ prob7-1.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+class Problem7_1 {
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+        Description:
+        
+        Complexity:
+        
+    */
+    public static void method1(int x) {
+        return;
+    }
+}
