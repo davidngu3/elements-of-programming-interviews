771590858958dff2594779e6bc21a3d9cda88c25
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-29 15:02:47 +1100

    Added problems 7.2 and 7.4

---
+++ prob7-2.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+class ProblemX {
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+        Description:
+        
+        Complexity:
+        
+    */
+    public static void method1(int x) {
+        return;
+    }
+}
