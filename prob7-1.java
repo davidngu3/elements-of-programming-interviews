6b01830ebdf20df3f977605e0fbdae3bef9eb075
davidngu3 <d.nguyen.melb@gmail.com>
2021-10-29 14:21:38 +1100

    int to string conversion

--- prob7-1.java
+++ prob7-1.java
@@ -4,17 +4,43 @@ import java.util.Random;
 
 class Problem7_1 {
     public static void main(String[] args) {
-        
+        System.out.println(int_to_string(123));
     }
 
     /*
         Description:
-            Reverse a string using brute force algo more comment and
+            Convert a string to int
         
         Complexity:
         
     */
-    public static void method1(int x) {
-        return;
+    public static int string_to_int(String x) {
+        return 0; 
     }
+
+    /*
+        Description:
+            Convert an int to string
+        
+        Complexity:
+        
+    */
+    public static String int_to_string(int x) {
+        StringBuilder sb = new StringBuilder();
+        
+        boolean isNegative = (x < 0) ? true : false;
+
+        while (x > 0) {
+            int dig = x % 10;
+            x = x / 10;
+            sb.insert(0, dig);
+        }
+
+        if (isNegative) {
+            sb.insert(0, '-');
+        }
+
+        return sb.toString();
+    }
+
 }
