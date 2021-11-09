22a1a0a462a7b0fcdd5bc69a8777bf3471985f01
davidngu3 <d.nguyen.melb@gmail.com>
2021-11-09 16:15:05 +1100

    7-4

--- prob7-4.java
+++ prob7-4.java
@@ -5,11 +5,25 @@ class Problem7_4 {
 
     /*
         Description:
+            First, replace b and count how many a's there are.
+            From this, we can calculate the expected size of the output string
+
+            Then, read backwards through the original array,
+                while copying to index e-1, where e is the expected size of the output string (so that the original array is not overwritten),
+                    writing 2 d's when there is an a
         
-        Complexity:ssss
+        Complexity: O(n)
         
     */
-    public static void replaceAndRemove(int x) {
+    public static void replaceAndRemove(char[] s) {
+        // first pass to remove b's
+        // 2 pointers, one write one read index
+        for (int i = 0; i < s.length; ++i) {
+            if (s[i] == 'b') {
+                s[i] = ' ';
+            }
+        }        
+
         return;
     }
 }
