1da52b4afaf45c88a0607c7e6216ac9c68aafcc5
davidngu3 <d.nguyen.melb@gmail.com>
2021-11-10 20:46:13 +1100

    palindrome

--- prob7-5.java
+++ prob7-5.java
@@ -1,17 +1,33 @@
 class Problem7_5 {
     public static void main(String[] args) {
+        System.out.println(testPalindromicity("rAce%@ ca!r"));
     }
 
     /*
         Description:
-           First strip all but letters
-           Then 1 pointer start, 1 pointer end
-           check equality until cross over
+           2 pointers
         
         Complexity: O(n)
         
     */
     public static boolean testPalindromicity(String s) {
-       return true;
+        // convert to lowercase letters only
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            char currChar = s.charAt(i);
+            if ((currChar >= 'a' && currChar <= 'z' ) || (currChar >= 'A' && currChar <= 'Z')) {
+                sb.append(currChar);
+            }
+        }
+        String s2 = sb.toString().toLowerCase();
+
+        // check palindromicity using 2 pointers
+        for (int l = 0, r = s2.length() - 1; l < r; l++, r--) {
+            if (s2.charAt(l) != s2.charAt(r)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
