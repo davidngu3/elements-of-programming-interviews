b307e864a99512b314c64e6234fa05c6bad93ff7
davidngu3 <d.nguyen.melb@gmail.com>
2021-11-10 20:03:17 +1100

    75

---
+++ prob7-5.java
@@ -0,0 +1,17 @@
+class Problem7_5 {
+    public static void main(String[] args) {
+    }
+
+    /*
+        Description:
+           First strip all but letters
+           Then 1 pointer start, 1 pointer end
+           check equality until cross over
+        
+        Complexity: O(n)
+        
+    */
+    public static boolean testPalindromicity(String s) {
+       return true;
+    }
+}
