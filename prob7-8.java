af7648501f2e4f0036b60394e9a5b03b53896571
davidngu3 <d.nguyen.melb@gmail.com>
2021-11-15 19:03:29 +1100

    szs

---
+++ prob7-8.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+class Problem7_8 {
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+        Description:
+        
+        Complexity:sss
+        
+    */
+    public static String lookSay(int n) {
+    
+    }
+
+}
