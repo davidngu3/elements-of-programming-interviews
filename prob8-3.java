94859f7d943b388835630d82f8bb2a4a91035e4f
davidngu3 <d.nguyen.melb@gmail.com>
2021-11-26 15:49:20 +1100

    cycle detect

--- prob8-3.java
+++ prob8-3.java
@@ -4,48 +4,65 @@ import java.io.*;
 
 class Problem8_3 {
     public static void main(String[] args) {
-        LinkedList<Integer> l1 = new LinkedList<Integer>();
-        l1.add(1);
-        l1.add(3);
-        l1.add(7);
-        l1.add(9);
-        l1.add(16);
+        ListNode<Integer> l0 =  new ListNode<Integer>(12, null);
+        ListNode<Integer> l1 = new ListNode<Integer>(11, null);
+        ListNode<Integer> l2 = new ListNode<Integer>(3, null);
+        ListNode<Integer> l3 = new ListNode<Integer>(5, null);
+        ListNode<Integer> l4 = new ListNode<Integer>(7, null);
+        ListNode<Integer> l5 = new ListNode<Integer>(2, null);
+        
+        l0.next = l1;
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+        l5.next = l2;
 
-        LinkedList<Integer> l2 = new LinkedList<Integer>();
-        l2.add(2);
-        l2.add(4);
-        l2.add(9);
-        l2.add(15);
-        l2.add(15);
-        l2.add(20);
-
-        mergeTwoSortedLL(l1, l2);
-
+        System.out.println(hasCycle(l0));
     }
     
-    public static LinkedList<Integer> mergeTwoSortedLL(LinkedList<Integer> l1, LinkedList<Integer> l2) {
-        Integer l1_idx = 0;
-        Integer l2_idx = 0;
+    // uses floyds cycle detection algorithm
+    public static ListNode<Integer> hasCycle(ListNode<Integer> head) {
         
-        while (l1_idx < l1.size() || l2_idx < l2.size()) {
-            if (l1_idx > l1.size() - 1) { // l1 index out of bound
-                l1.add(l2.get(l2_idx));
-                l2_idx++;
+        // two pointers, slow and fast
+        ListNode<Integer> slow = head;
+        ListNode<Integer> fast = head;
+
+        boolean cycleDetected = false;
+        int cycleLength = 0;
+
+        while (fast.next != null && fast.next.next != null) {
+            if (cycleDetected) {
+                cycleLength++;
             }
 
-            else if (l2_idx > l2.size() - 1) { // l2 index out of bound
-                l1_idx++;
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) { // cycle detected, find cycle head
+
+                if (cycleDetected == true) { // 2nd time collision: find head of cycle;
+                    ListNode<Integer> temp = head;
+                    int i = 0;
+                    while (i++ < cycleLength) {
+                        temp = temp.next;
+                    }
+
+                    // move head and temp equivalently to find head of cycle
+                    while (head != temp) {
+                        head = head.next;
+                        temp = temp.next;
+                    }
+
+                    return head;
+                }
+                // find length of cycle
+                cycleDetected = true;
+
             }
 
-            else if (l1.get(l1_idx) < l2.get(l2_idx)) { // l1 < l2
-                l1_idx++;
-            }
-            else if (l1.get(l1_idx) >= l2.get(l2_idx)) { // l1 > l2
-                l1.add(l1_idx, l2.get(l2_idx));
-                l2_idx++;
-            }
         }
 
-        return l1;
+        return new ListNode<Integer>(-1, null); // if fast reaches null, then no cycle exists (fast >> slow)
     }
 }
