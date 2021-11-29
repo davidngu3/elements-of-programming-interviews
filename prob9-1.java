26826e172154e85781c11c592d35d027adf69806
davidngu3 <d.nguyen.melb@gmail.com>
2021-11-30 10:27:24 +1100

    9-1

--- prob9-1.java
+++ prob9-1.java
@@ -4,13 +4,33 @@ import java.io.*;
 
 class Problem9_1 {
     public static void main(String[] args) {
-       
-        
-    }
-    
-    public static void max(LinkedList<Integer> s) {
-       
+        LinkedList<Integer> stack = new LinkedList<>();
+        stack.push(5);
+        stack.push(6);
+        stack.push(2);
+        stack.push(4);
+
+        System.out.println(max(stack));
+
     }
 
-    
+    public static Integer max(LinkedList<Integer> s) {
+        LinkedList<Integer> tempStack = new LinkedList<>();
+
+        int max = Integer.MIN_VALUE;
+        while (!s.isEmpty()) {
+            int curr = s.pop();
+            tempStack.push(curr);
+            if (curr > max) {
+                max = curr;
+            }
+        }
+
+        while (!tempStack.isEmpty()) {
+            s.push(tempStack.pop());
+        }
+        
+        return max;
+    }
+
 }
