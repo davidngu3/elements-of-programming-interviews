905f01bb924b2a840862f289b126c18d9f6b2773
davidngu3 <d.nguyen.melb@gmail.com>
2021-11-30 09:51:27 +1100

    9-1 template

---
+++ prob9-1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
+
+class Problem9_1 {
+    public static void main(String[] args) {
+       
+        
+    }
+    
+    public static void max(LinkedList<Integer> s) {
+       
+    }
+
+    
+}
