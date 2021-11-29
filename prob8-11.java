93275db0e113bf72ed164e92c159b38899ed39d7
davidngu3 <d.nguyen.melb@gmail.com>
2021-11-29 23:26:38 +1100

    linked list palindrome

--- prob8-11.java
+++ prob8-11.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 import java.util.*;
 import java.io.*;
 
-class Problem8_10 {
+class Problem8_11 {
     public static void main(String[] args) {
         ListNode<Integer> l1 = new ListNode<Integer>(1, null);
         ListNode<Integer> l3 = new ListNode<Integer>(2, null);
         ListNode<Integer> l4 = new ListNode<Integer>(3, null);
-        ListNode<Integer> l5 = new ListNode<Integer>(4, null);
-        ListNode<Integer> l6 = new ListNode<Integer>(5, null);
-        ListNode<Integer> l7 = new ListNode<Integer>(6, null);
+        ListNode<Integer> l5 = new ListNode<Integer>(3, null);
+        ListNode<Integer> l6 = new ListNode<Integer>(2, null);
+        ListNode<Integer> l7 = new ListNode<Integer>(1, null);
             
         l1.next = l3;
         l3.next = l4;
@@ -18,10 +18,51 @@ class Problem8_10 {
         l6.next = l7;
 
         System.out.println(isPalindromic(l1));
+        
     }
     
-    public static ListNode<Integer> isPalindromic(ListNode<Integer> head) {
-      
-        return new ListNode<Integer>(0, null);
+    public static boolean isPalindromic(ListNode<Integer> head) {
+        
+        // reverse list
+        ListNode<Integer> reverse = reverseList(head);
+        
+        while (reverse != null && head != null) {
+            if (reverse == null || head == null) { // one list shorter than the other
+                return false;
+            } 
+            if (reverse.data != head.data) { // unequal
+                return false;
+            }
+            head = head.next;
+            reverse = reverse.next;
+        }  
+        return true;
+
     }
+
+    public static ListNode<Integer> reverseList(ListNode<Integer> head) {
+        
+        // reverse list
+        ListNode<Integer> dummy = new ListNode<Integer>(0, head);
+
+        ListNode<Integer> prev = dummy;
+        ListNode<Integer> curr = head;
+        ListNode<Integer> temp;
+
+        while (curr != null) {
+            temp = curr.next;
+            curr.next = prev;
+
+            prev = curr;
+            curr = temp;
+        }
+
+        head.next = null;
+
+        // check palindromicity
+
+        return prev;
+    }
+
+    
 }
