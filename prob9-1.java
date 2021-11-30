6fe6d847b1bfc8ac7e064ac4cb82c91ef9ba794f
davidngu3 <d.nguyen.melb@gmail.com>
2021-11-30 13:12:07 +1100

    max stack api

--- prob9-1.java
+++ prob9-1.java
@@ -4,33 +4,46 @@ import java.io.*;
 
 class Problem9_1 {
     public static void main(String[] args) {
-        LinkedList<Integer> stack = new LinkedList<>();
-        stack.push(5);
-        stack.push(6);
-        stack.push(2);
-        stack.push(4);
+        Stack s1 = new Stack();
+        s1.push(5);
+        s1.push(2);
+        s1.push(1);
+        s1.push(9);
+        s1.push(3);
 
-        System.out.println(max(stack));
-
-    }
-
-    public static Integer max(LinkedList<Integer> s) {
-        LinkedList<Integer> tempStack = new LinkedList<>();
-
-        int max = Integer.MIN_VALUE;
-        while (!s.isEmpty()) {
-            int curr = s.pop();
-            tempStack.push(curr);
-            if (curr > max) {
-                max = curr;
-            }
-        }
-
-        while (!tempStack.isEmpty()) {
-            s.push(tempStack.pop());
-        }
+        System.out.println(s1.max());
         
-        return max;
+        s1.pop();
+        s1.pop();
+
+        System.out.println(s1.max());
+    }
+}
+
+class Stack {
+    private Deque<Integer> stack;
+    private Deque<Integer> maxStack;
+
+    public Stack() {
+        stack = new LinkedList<Integer>();
+        maxStack = new LinkedList<Integer>();
     }
 
+    public void push(Integer i) {
+        if (stack.isEmpty() || i > maxStack.peek()) {
+            maxStack.push(i);
+        }
+        stack.push(i);
+    }
+
+    public Integer pop() {
+        if (stack.peek() == maxStack.peek()) {
+            maxStack.pop();
+        }
+        return stack.pop();
+    }
+    
+    public Integer max() {
+        return maxStack.peek();
+    }
 }
