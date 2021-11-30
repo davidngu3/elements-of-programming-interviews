42eea13a8f6b03629e631855ca73b2a53a193109
davidngu3 <d.nguyen.melb@gmail.com>
2021-11-30 15:58:27 +1100

    s

---
+++ prob9-2.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
+
+// alternate solution:
+//    each node in the stock stores additional data, namely the maximum in the substack below it
+
+
+class Problem9_2 {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void evaluateRPN(String exp) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+
+    }
+}
