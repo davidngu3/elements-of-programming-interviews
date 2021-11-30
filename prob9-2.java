bbb38637504c1df38f1fe08f985694870058c35a
davidngu3 <d.nguyen.melb@gmail.com>
2021-11-30 16:14:34 +1100

    RPN expression

--- prob9-2.java
+++ prob9-2.java
@@ -8,11 +8,34 @@ import java.io.*;
 
 class Problem9_2 {
     public static void main(String[] args) {
-        
+        System.out.println(evaluateRPN("3,4,x,2,3,+,+"));
     }
 
-    public static void evaluateRPN(String exp) {
+    public static Integer evaluateRPN(String exp) {
         Deque<Integer> stack = new LinkedList<Integer>();
 
+        String[] symbols = exp.split(",");
+
+        for (String token : symbols) {
+            if ("+-x/".contains(token)) { // operator found
+                Integer s2 = stack.removeFirst();
+                Integer s1 = stack.removeFirst();
+
+                if (token.equals("+")) {
+                    stack.addFirst(s1 + s2);
+                } else if (token.equals("-")) {
+                    stack.addFirst(s1 - s2);
+                } else if (token.equals("x")) {
+                    stack.addFirst(s1 * s2);
+                } else if (token.equals("/")) {
+                    stack.addFirst(s1 / s2);
+                }
+            }
+            else { 
+                stack.addFirst(Integer.parseInt(token));
+            }
+        }
+
+        return stack.removeFirst();
     }
 }
