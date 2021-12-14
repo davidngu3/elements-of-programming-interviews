69ab8ffc7d8d23d873cbfc186a2d0aa221201533
David <d.nguyen.melb@gmail.com>
2021-12-14 17:32:58 +1100

    inorder

--- prob10-11.java
+++ prob10-11.java
@@ -24,23 +24,70 @@ class Problem10_11 {
         d.left = f;
         f.parent = d;
 
+        // additional test
+        TreeNodeWithParent g = new TreeNodeWithParent(7, null, null);
+        TreeNodeWithParent h = new TreeNodeWithParent(8, null, null);
+        TreeNodeWithParent i = new TreeNodeWithParent(9, null, null);
+        g.parent = c;
+        h.parent = g;
+        i.parent = h;
+
+        c.left = g;
+        g.left = h;
+        h.right = i;
+
         /* Tree structure
-                    a1
-                  /   \
-                 b2     c3
-                / \ 
-               d4   e5
-              /
-             f6
+                     a1
+                  /     \
+                 b2       c3
+                / \      /
+               d4  e5  g7
+              /       /
+             f6      h8
+                      \
+                        i9
         */
         inOrderTraversal(a);
     }
 
     public static void inOrderTraversal(TreeNodeWithParent l1) {
-        // traverse down left subtree
+        boolean terminate = false;
+        boolean comeFromLeft = true;
+
+        // go to starting node
         while (l1.left != null) {
             l1 = l1.left;
-            System.out.println(l1.data);
+        }
+        
+        TreeNodeWithParent n = l1;
+
+        while (!terminate) { 
+            if (comeFromLeft) { // if come from left, process, check right
+                System.out.println(n.data);
+
+                if (n.right != null) {
+                    n = n.right;
+                    // go to leftmost  node
+                    while (n.left != null) {
+                        n = n.left;
+                    }
+                }
+                else {
+                    TreeNodeWithParent p = n.parent;
+                    comeFromLeft = (n == p.left) ? true : false;
+                    n = n.parent;
+                }
+            }
+            else { // if come from right, go up
+                if (n.parent == null) {
+                    terminate = true;
+                }
+                else {
+                    TreeNodeWithParent p = n.parent;
+                    comeFromLeft = (n == p.left) ? true : false;
+                    n = n.parent;
+                }
+            }
         }
     }
 }
