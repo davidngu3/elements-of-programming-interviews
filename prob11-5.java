b2cd3bbd073d87953f0c9988871efa9629427446
David <d.nguyen.melb@gmail.com>
2021-12-20 14:26:23 +1100

    add 11-5

---
+++ prob11-5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Problem11_5 {
+    public static void main(String[] args) {
+     
+    }
+
+    public static void findMedian(Iterator<Integer> data) {
+        
+    }
+
+}
