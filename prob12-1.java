b8beec6c569294e2e9a30430854d2ba7f42025e4
David <d.nguyen.melb@gmail.com>
2021-12-21 11:13:22 +1100

    binary search first occurrence optimized

--- prob12-1.java
+++ prob12-1.java
@@ -4,6 +4,10 @@ import java.util.*;
 // binary search until key is found
 // scan left until first occurrence found
 
+// better stragey:
+// binary serach until key is found, store as candidate
+// continue binary search to the left until better candidate found; 
+
 class Problem12_1 {    
     public static void main(String[] args) {
         System.out.println(searchFirstOccurrence(Arrays.asList(1, 2, 5, 5, 5, 9, 10), 5));
@@ -11,38 +15,29 @@ class Problem12_1 {
 
     public static int searchFirstOccurrence(List<Integer> arr, int key) {
         // binary search
-        int pos = binarySearch(arr, key);
-
-        // doesnt exist in array
-        if (pos == -1) {
-            return -1;
-        }
-
-        while (pos > 0 && arr.get(pos-1) == arr.get(pos)) {
-            pos--;
-        }
-
-        return pos;
+        return binarySearch(arr, key);
     }
 
     public static int binarySearch(List<Integer> arr, int k) {
         int L = 0;
         int R = arr.size() - 1;
+        int candidate = arr.size();
 
         while (L <= R) {
-            int M = L + (R - L) / 2;
+            int M = L + ((R - L) / 2);
 
             if (arr.get(M) < k) {
                 L = M + 1;
             }
             else if (arr.get(M) == k) {
-                return M;
+                candidate = Math.min(M, candidate); // update candidate
+                R = M - 1;
             }
             else {
                 R = M - 1;
             }
         }
 
-        return -1;
+        return candidate == arr.size() ? -1 : candidate;
     }
 }
