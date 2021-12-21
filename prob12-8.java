f9ce336a670c0ebdb3adae094a60deb32419ac1a
David <d.nguyen.melb@gmail.com>
2021-12-21 13:45:48 +1100

    kth largest element setup

---
+++ prob12-8.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+// strategy
+
+class Problem12_8 {    
+    public static void main(String[] args) {
+        System.out.println(kthLargestElement(new ArrayList<Integer>(Arrays.asList(1, 6, 2, 5, 2, 10)), 3));
+    }
+
+    public static int kthLargestElement(List<Integer> arr, int k) {
+       return 0;
+    }
+}
