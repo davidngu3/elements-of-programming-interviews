9386ebb0adc3f1bc72a84e9d85aa8d32bfb7c908
David <d.nguyen.melb@gmail.com>
2021-12-25 14:21:49 +1100

    search cyclic sorted

--- prob12-3.java
+++ prob12-3.java
@@ -3,11 +3,33 @@ import java.util.*;
 // strategy
 // binary search, using condition that the chosen element must be smaller than the element to its left.
 
-class Problem12_3 {    
+class Problem12_3 {
     public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList(16, 1, 2, 3));
+        System.out.println(searchCyclicSortedArray(arr));
     }
 
-    public static void searchCyclicSortedArray() {
-       
+    public static int searchCyclicSortedArray(List<Integer> arr) {
+        int L = 0;
+        int R = arr.size() - 1;
+
+        while (L <= R) {
+            int M = L + (R - L) / 2;
+            
+            if (arr.get(M) < arr.get(M-1)) { // target
+                return arr.get(M);
+            }
+            else if (arr.get(M) < arr.get(L)) { // take left
+                R = M - 1;
+            }
+            else if (arr.get(R) < arr.get(M)) { // take right
+                L = M + 1;
+            }
+            else { // array must be sorted, so take element 0
+                return arr.get(L);
+            }
+        }
+
+        return -1;
     }
 }
