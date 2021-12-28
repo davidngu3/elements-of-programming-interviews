e30eecb7603e530d11a1542e16f7417c624d5e12
David <d.nguyen.melb@gmail.com>
2021-12-28 11:05:57 +1100

    anonymous letter construction

--- prob13-2.java
+++ prob13-2.java
@@ -1,3 +1,36 @@
+import java.util.*;
+
 class Problem13_2 {
-    
+    public static void main(String[] args) {
+        System.out.println(canBeConstructed("hello", "hey elephant loser"));
+    }
+
+    // letter can be constructed by magazine if characters in letter is a subset of magazine
+    // store magazine characters in hashmap, then iterate letter and decrement
+    public static boolean canBeConstructed(String letter, String magazine) {
+        HashMap<Character, Integer> availableLetters = new HashMap<>();
+
+        for (char c : letter.toCharArray()) {
+            if (availableLetters.containsKey(c)) {
+                availableLetters.put(c, availableLetters.get(c)+1);
+            }
+            else {
+                availableLetters.put(c, 1);
+            }
+        }
+
+        for (char c : magazine.toCharArray()) {
+            if (!availableLetters.containsKey(c) || availableLetters.get(c) <= 0) {
+                return false;
+            }
+            else {
+                availableLetters.put(c, availableLetters.get(c)-1);
+            }
+        }
+
+
+        return true;
+
+
+    }
 }
