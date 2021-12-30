7f48654c04e84b99a695b3035448acae08af7387
David <d.nguyen.melb@gmail.com>
2021-12-30 19:44:19 +1100

    14-16

--- prob14-6.java
+++ prob14-6.java
@@ -7,11 +7,19 @@ strategy:
 
 class Problem14_6 {
     public static void main(String[] args) {
-      
+
     }
 
-    public static int computeUnionOfIntervals(List<Interval> intervals) { 
-      
+    public static List<Interval> computeUnionOfIntervals(List<Interval> intervals) {
+        // sort intervals by start time, break ties by prioritizing closed intervals
+        // continuously build intervals, analyzing how to augment the interval using the fed in intervals
+        // e.g. if next intervals starts after current interval ends, build new interval
+        // e.g. if nmext interval extends current interval, update projected endpoint
+
+        List<Interval> ret = new ArrayList<Interval>();
+
+
+        return ret;
     }
 }
 
