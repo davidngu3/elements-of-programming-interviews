eb8c448451b06fde1c0da4e0fc39009fbb41b22b
David <d.nguyen.melb@gmail.com>
2022-01-01 15:32:32 +1100

    check valid BST

--- prob15-1.java
+++ prob15-1.java
@@ -1,9 +1,40 @@
 class Problem15_1 {
     public static void main(String[] args) {
+        TreeNode head = new TreeNode(5, null, null);
+        TreeNode a = new TreeNode(3, null, null);
+        TreeNode b = new TreeNode(7, null, null);
+        TreeNode c = new TreeNode(1, null, null);
+        TreeNode e = new TreeNode(4, null, null);
+        TreeNode f = new TreeNode(2, null, null);
+        TreeNode d = new TreeNode(9, null, null);
 
+        head.left = a;
+        head.right = b;
+        a.left = c;
+        a.right = e;
+        e.left = f;
+        b.right = d;
+
+        System.out.println(isValidBST(head));
     }
 
     public static boolean isValidBST(TreeNode head) {
-        return false;
+        if (head == null) {
+            return true;
+        }
+
+        // check BST conditions
+        if (head.left != null) {
+            if (head.left.data > head.data) {
+                return false;
+            }
+        }
+        if (head.right != null) {
+            if (head.right.data < head.data) {
+                return false;
+            }
+        }
+
+        return isValidBST(head.left) && isValidBST(head.right);
     }
 }
