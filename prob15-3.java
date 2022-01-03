534079e4138c03f379c363a994d8c22ae312c2e5
David <d.nguyen.melb@gmail.com>
2022-01-03 22:36:20 +1100

    s

---
+++ prob15-3.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+/* strategy
+    send a pointer k elements ahead using: prioritising right then left
+    both pointers traverse, priotising right then left until fast pointer reachess null node
+
+*/
+class Problem15_3 {
+    public static void main(String[] args) {
+       
+    }
+
+
+    public static List<Integer> findKthLargestElements(TreeNode root, int k) {
+        // send fast pointer k ahead
+        TreeNode fast = root;
+
+        // move both pointers in tandem until fast hits null
+
+        // move slow pointer, adding nodes to array
+
+        return new ArrayList<Integer>(Arrays.asList(0)); 
+    }
+    
+    public static TreeNode search(TreeNode n) {
+        
+
+
+        return new TreeNode(0, null, null);
+    }
+}
