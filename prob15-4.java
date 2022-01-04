5c1d5e4b90291eec0180355aee81d647f8d2396e
David <d.nguyen.melb@gmail.com>
2022-01-04 12:12:10 +1100

    LCA

--- prob15-4.java
+++ prob15-4.java
@@ -1,6 +1,10 @@
 import java.util.*;
 
 /* strategy
+    all keys are distinct
+    find the key with a value between values of a and b?
+    is there only one key with this value? no
+        but: the required one is the first key that exhibits this behaviour
 
 */
 class Problem15_4 {
@@ -20,11 +24,34 @@ class Problem15_4 {
         e.left = f;
         b.right = d;
 
-        LCA(head, b, c);
+        TreeNode res = LCA(head, c, e);
+        System.out.println(res.data);
     }
 
-
     public static TreeNode LCA(TreeNode root, TreeNode a, TreeNode b) {
-        return new TreeNode(5, null, null);
+        if (a.data < b.data) {
+            return LCAaux(root, a, b);
+        }
+        else {
+            return LCAaux(root, b, a);
+        }
+    }
+
+    public static TreeNode LCAaux(TreeNode root, TreeNode a, TreeNode b) {
+        if (root != null) {
+            if (root.data >= a.data && root.data < b.data) {
+                return root;
+            }
+    
+            if (root.data < a.data) {
+                return LCAaux(root.right, a, b);
+            }
+    
+            if (root.data > b.data) {
+                return LCAaux(root.left, a, b);
+            }
+        }
+
+        return new TreeNode(0, null, null); // wont be hit?
     }
 }
