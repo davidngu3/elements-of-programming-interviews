637642c908d526548f174285062bb466658e78eb
David <d.nguyen.melb@gmail.com>
2022-01-04 11:49:53 +1100

    k largest

--- prob15-3.java
+++ prob15-3.java
@@ -1,31 +1,46 @@
 import java.util.*;
 
 /* strategy
-    send a pointer k elements ahead using: prioritising right then left
-    both pointers traverse, priotising right then left until fast pointer reachess null node
+    reverse postorder traversal, stopping when k elements added to result
 
 */
 class Problem15_3 {
     public static void main(String[] args) {
-       
+        TreeNode head = new TreeNode(5, null, null);
+        TreeNode a = new TreeNode(3, null, null);
+        TreeNode b = new TreeNode(7, null, null);
+        TreeNode c = new TreeNode(1, null, null);
+        TreeNode e = new TreeNode(4, null, null);
+        TreeNode f = new TreeNode(2, null, null);
+        TreeNode d = new TreeNode(9, null, null);
+
+        head.left = a;
+        head.right = b;
+        a.left = c;
+        a.right = e;
+        e.left = f;
+        b.right = d;
+
+        List<Integer> res = findKthLargestElements(head, 5);
+        for (int i : res) {
+            System.out.println(i);
+        }
     }
 
 
     public static List<Integer> findKthLargestElements(TreeNode root, int k) {
-        // send fast pointer k ahead
-        TreeNode fast = root;
-
-        // move both pointers in tandem until fast hits null
-
-        // move slow pointer, adding nodes to array
-
-        return new ArrayList<Integer>(Arrays.asList(0)); 
-    }
+        ArrayList<Integer> arr = new ArrayList<>();
+        findKthLargestElementsAux(root, k, arr);
+        return arr;
+    }   
     
-    public static TreeNode search(TreeNode n) {
-        
-
-
-        return new TreeNode(0, null, null);
+    public static void findKthLargestElementsAux(TreeNode n, int k, List<Integer> arr) {
+        if (n != null && arr.size() < k) {
+            findKthLargestElementsAux(n.right, k, arr); // process right subtree
+            if (arr.size() < k) {
+                arr.add(n.data); // process root
+                findKthLargestElementsAux(n.left, k, arr); // process left
+            }
+        }
     }
 }
